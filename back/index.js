f6ce75c246b8d10e67f9c1c922c9180a391bdc1e
Julian <julian.meneses11@gmail.com>
2023-07-10 17:59:07 -0300

    comentarios extras

--- back/index.js
+++ back/index.js
@@ -12,7 +12,7 @@ const port = 3900;
 // Configurar cors (esto es un middleware, se va a ejecutar antes de cada petición HTTP)
 app.use(cors());
 
-// Convertir el body de cualquier petición HTTP a un objeto js
+// Convertir el body de cualquier petición HTTP a un objeto js (estos también son middlewares)
 app.use(express.json({limit: '4mb'})); // esto se aplica cuando el content-type de los datos sea json
 app.use(express.urlencoded({extended:true})); // para content-type igual a x-www-form-urlencoded
 
